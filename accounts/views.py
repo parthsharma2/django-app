a8a1f0a2e4bc15decd1aaea230ba71ab7e4d5154
Parth Sharma <parth261297@gmail.com>
2017-07-17 20:53:30 +0530

    Added static files and login template

--- accounts/views.py
+++ accounts/views.py
@@ -6,4 +6,4 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 
 def home(request):
-    return render(request, 'login.html')
+    return render(request, 'accounts/home.html')
